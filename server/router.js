ffebca96eb61ad6d9ef7185c6ac0fe995d19f795
Joshua Haber <jxh6709@rit.edu>
2021-03-08 21:03:26 -0500

    Update router.js

--- server/router.js
+++ server/router.js
@@ -26,7 +26,7 @@ const router = (app) => {
         // The "catchall" handler: for any request that doesn't
     // match one above, send back React's index.html file.
     app.get('*', (req, res) => {
-      res.sendFile(path.join(__dirname+'../dist/A-Shore/index.html'));
+      res.sendFile(path.join(__dirname+'../../dist/A-Shore/index.html'));
     });
 
 
