53422f7398cf8a2fe38540c51dea9df73ccad09e
Zach <zab5957@rit.edu>
2021-03-11 09:34:45 -0500

    new branch start test

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { SignupComponent } from './pages/signup/signup.component';
+import { TestComponent } from './pages/test/test.component';
+//For secure login, we can use this later if we have time
+//import { AuthGuard } from './services/auth.guard';
 
-const routes: Routes = [];
+const routes: Routes = [
+  //change this route to login later once we setup auth
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
 
-@NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
-})
+  //if creating a new component put your routes here
+  { path: 'login', component: LoginComponent },
+  { path: 'signup', component: SignupComponent },
+  { path: 'home', component: HomeComponent},
+  { path: 'home', component: HomeComponent},
+];
+  @NgModule({
+    imports: [RouterModule.forRoot(routes, { useHash: true, relativeLinkResolution: 'legacy', scrollPositionRestoration: 'enabled'  })],
+    exports: [RouterModule]
+  })
 export class AppRoutingModule { }
---
+++ src/app/pages/login/login.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+
+  constructor(private router: Router, private toastr: ToastrService) { }
+
+  ngOnInit(): void {
+  }
+}
