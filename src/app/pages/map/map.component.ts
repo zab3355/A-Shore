11f52df13365c0ac8b521e195055f2d6dceaea16
jzimmer5 <jrz4547@rit.edu>
2021-03-16 15:13:24 -0400

    Setup like the last one

--- src/app/pages/map/map.component.ts
+++ src/app/pages/map/map.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-let map: google.maps.Map;
+
 /// <reference types="google.maps" />
 
 @Component({
@@ -9,20 +9,38 @@ let map: google.maps.Map;
 })
 export class MapComponent implements OnInit {
 
-  constructor() { }
+  
+
+  constructor() { 
+    
+  }
 
   ngOnInit(): void {
   }
 
+  addMarker(location: google.maps.LatLngLiteral, map: google.maps.Map) {
+    // Add the marker at the clicked location, and add the next-available label
+    // from the array of alphabetical characters.
+    new google.maps.Marker({
+      position: location,
+      label: String(this.labelIndex++),
+      animation: google.maps.Animation.DROP,
+      map: map,
+    });
+  }
+  
   mapRestrictions = {
     north: 90,
     south: -90,
     west: 180,
     east: 0,
-};
+    };
+  labelIndex = 1;
 
-  initMap(): void {
-  map = new google.maps.Map(document.getElementById("map") as HTMLElement, {
+  interactiveCoordinates = [];
+
+   initMap(): void {
+    let map = new google.maps.Map(document.getElementById("map") as HTMLElement, {
     center: { lat: 10, lng: -20.744857 },
     restriction: {
       latLngBounds: this.mapRestrictions,
@@ -30,5 +48,23 @@ export class MapComponent implements OnInit {
     },
     zoom: 3,
   });
-}
+    this.getLocations(map);
+  }
+  
+  getLocations(map: google.maps.Map){
+
+    //this.addMarker( ,map)
+
+    let drawPath = new google.maps.Polyline({
+      path: this.interactiveCoordinates,
+      geodesic: true,
+      strokeColor: "#FF0000",
+      strokeOpacity: 1.0,
+      strokeWeight: 2,
+    });
+
+    drawPath.setMap(map);
+  }
+
+
 }
